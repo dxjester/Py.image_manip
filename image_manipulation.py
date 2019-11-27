85a11caba533fc3b1d80eadc5000a4f6dea206c3
dxjester <patbenitez@gmail.com>
2019-11-27 18:54:29 -0500

    added displayImage() functionto the class

--- image_manipulation.py
+++ image_manipulation.py
@@ -12,7 +12,7 @@ VERSION: 1.0
 #--------------------------- PHASE 1: Program Setup --------------------------#
 #-----------------------------------------------------------------------------#
 
-
+# 1.1: Module import ---------------------------------------------------------#
 # import the required modules
 import pandas as pd
 import numpy as np
@@ -27,14 +27,27 @@ import plot_functions as pf
 # from collections import Counter
 # from string import punctuation
 
-
+# 1.2: Class Declaration -----------------------------------------------------#
 class image_file: 
     
     def __init__ (self, filename, file_ext):
         self.filename = filename
         self.extension = file_ext
-        self.image_data = PIL.Image.open('laptop.jpg')
+        self.image_data = PIL.Image.open(filename)
         self.image_array = np.array(self.image_data)
     
-    def return_array():
+    def display_image(self):
+        self.image_data.show()
         
+    def return_array(self):
+        return self.image_array
+    
+#----------------------------------- START -----------------------------------#
+#----------------------- PHASE 2: Program Execution --------------------------#
+#-----------------------------------------------------------------------------#
+        
+laptop = image_file('laptop.jpg', 'jpg') # import 'laptop.jpg' file in local directory
+laptop.display_image() # display the image
+
+laptop_array = laptop.return_array()
+
